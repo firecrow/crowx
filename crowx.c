db4b0094b4189578daffdb8279ed4099d4f8d2da
firecrow <fire@firecrow.com>
2019-04-18 14:23:04 -0400

    filling in somre more features

--- crowx.c
+++ crowx.c
@@ -1,15 +1,16 @@
 
-void xerrlog(char *str){
-    fprintf(stderr, str);
-}
+#ifndef xerrlog 
+    #define xerrlog(x) \
+        fprintf(stderr, (x))
+#endif
 
 #define xok(x) \
     if(x) \
-        xerrlog(stderr, "NOT OK %s:%s:%s %s\n", __file__, __line__, __FUNC__, #x)
+        xerrlog(stderr, "NOT OK %s:%s:%s %s\n", __FILE__, __LINE__, __func__, #x)
 
 #define xptrok(x) (!(x))
     if((x) == NULL) \
-        xerrlog(stderr, "PTR NOT OK %s:%s:%s %s\n", __file__, __line__, __FUNC__, #x)
+        xerrlog(stderr, "PTR NOT OK %s:%s:%s %s\n", __FILE__, __LINE__, __func__, #x)
 
 char *xdup(char *x, size_t s){
     char *r;
