b36a4654eb93fa1c16529701cc5540499be63c13
firecrow <fire@firecrow.com>
2019-05-05 19:29:53 -0400

    addinng commented sketch of tests strategy

--- crowx.c
+++ crowx.c
@@ -1,4 +1,3 @@
-
 #ifndef xerrlog 
     #define xerrlog(x) \
         fprintf(stderr, (x))
---
+++ test.c
@@ -0,0 +1,10 @@
+
+int main(){
+  /* xok success */;
+  /* xok failure (subp) with line number */
+  /* xprtok success */
+  /* xptrok fail with line number */
+  /* xdup */
+  /* xdupstr */
+  /* compile custom errlog function */
+}
