aa1953851ce037325b72d610e1753feccfb8175e
firecrow <fire@firecrow.com>
2019-05-13 20:52:30 -0400

    all working

--- crowx.c
+++ crowx.c
@@ -1,29 +1,30 @@
 char xerrmsg[1024];
 
 #ifndef xerrlog
-    #define xerrlog(x) do {\
-        fprintf(stderr, "%s", (x)); \
+    #define xerrlog(XARG) do {\
+        fprintf(stderr, "%s", (XARG)); \
         fflush(stderr); \
-        exit(1); \
+        exit(errno || 1); \
       } while(0)
 #endif
 
-#define xok(x) \
-    if(x) do {\
-        snprintf(xerrmsg, 1024, "NOT OK %s:%d:%s %d\n", __FILE__, __LINE__, __func__, x); \
+#define xok(XARG) \
+    if(XARG) do {\
+        snprintf(xerrmsg, 1024, "NOT OK %s:%d:%s %d\n", __FILE__, __LINE__, __func__, XARG); \
         xerrlog(xerrmsg); \
     } while(0)
 
-#define xokgt(x) \
-    if((x) < 0) do { \
-        snprintf(xerrmsg, 1024, "GT NOT OK %s:%d:%s %d\n", __FILE__, __LINE__, __func__, x); \
+#define xokgt(XARG) \
+    if((XARG) < 0) do { \
+        snprintf(xerrmsg, 1024, "GT NOT OK %s:%d:%s %d\n", __FILE__, __LINE__, __func__, XARG); \
         xerrlog(xerrmsg); \
     } while(0)
 
-/*
-#define xokptr(x) \
-    if((x) == NULL) \
-        xerrlog(stderr, "PTR NOT OK %s:%s:%s %s\n", __FILE__, __LINE__, __func__, #x)
+#define xokptr(XARG) \
+    if((XARG) == NULL) do { \
+        snprintf(xerrmsg, 1024, "PTR NOT OK %s:%d:%s\n", __FILE__, __LINE__, __func__); \
+        xerrlog(xerrmsg); \
+    } while(0)
 
 char *xdup(char *x, size_t s){
     char *r;
@@ -35,4 +36,3 @@ char *xdup(char *x, size_t s){
 char *xdupstr(char *x){
     return xdup(x, strlen(x));
 }
-*/
--- test.c
+++ test.c
@@ -1,10 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <errno.h>
 #include "crowx.c"
 
-#define exit(x) \
-  fprintf(stderr, "exit called for %d\n", x)
+#define exit(XARG) \
+  fprintf(stderr, "exit called for %d\n", XARG)
+  
+void *(*_malloc)(unsigned long s) = malloc;
+void *example = (void *)"hi";
+
+void *m(unsigned long XARG){
+    if(XARG == 1) 
+      return example; 
+    else 
+      return NULL;
+}
+
+#define malloc(XARG) m(XARG) 
 
 int main(){
 
@@ -13,12 +26,14 @@ int main(){
   xokgt(0);
   xokgt(10);
   xokgt(-1);
+  void *x;
+  xokptr(x = malloc(1));
+  xokptr(x = malloc(10));
+  char *a = "alpha bravo charlie delta echo";
+  char *b = "foxtrot golf hotel india juliet";
+  char *c = xdup(a, strlen(a));
+  xok(strncmp(a, c, strlen(a)));
+  char *d = xdupstr(b);
+  xok(strncmp(b, d, strlen(b)));
   
-  /* xok success */;
-  /* xok failure (subp) with line number */
-  /* xprtok success */
-  /* xptrok fail with line number */
-  /* xdup */
-  /* xdupstr */
-  /* compile custom errlog function */
 }
